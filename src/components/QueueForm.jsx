54bf584240a6e7d8c7965481a5274dea3f7977b0
lord-amresh <amresh2006@gmail.com>
2026-03-22 09:15:15 +0545

    continued...

--- src/components/QueueForm.jsx
+++ src/components/QueueForm.jsx
@@ -1,7 +1,25 @@
+import { useState } from "react";
+
 export default function QueueForm({onAdd}) {
+    const [name, setName] = useState("");
+    const [service, setService] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        //validations
+        if(!name.trim() || !service.trim()) return
+        onAdd({name,service})
+        setName("")
+        setService("")
+    }
+
     return(
         <>
-        <h1>QueueForm</h1>
+        <form className="queue-form" onSubmit={handleSubmit}>
+            <h2>Add to Queue</h2>
+            <div className="form-group"></div>
+            continued.......................
+        </form>
         </>
     )
 }
