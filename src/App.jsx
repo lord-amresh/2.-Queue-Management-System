f09319e5a77ddd3b012415e191edb4782dede37c
lord-amresh <amresh2006@gmail.com>
2026-03-22 08:57:57 +0545

    3 - Learn the data flow in components in react

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,31 @@
+import { useState } from "react";
 import "./App.css";
+import QueueForm from "./components/QueueForm";
 
 export default function App() {
+  const [queue, setQueue] = useState([]);
+
+  const addToQueue = (customer) => {
+    //add data to queue
+  };
+  const updateStatus = (id, newStatus) => {
+    //change data in queue
+  };
+  const removeFromQueue = (id) => {
+    //remove data from the queue
+  };
+
+
   return(
     <div className="app">
       <header>
         <h1>Queue Management Application</h1>
         <p>Manage your Customers Efficently</p>
       </header>
+      <main>
+        <QueueForm onAdd={addToQueue}/>
+        <h1>QueueDisplay</h1>
+      </main>
     </div>
   );
 }
